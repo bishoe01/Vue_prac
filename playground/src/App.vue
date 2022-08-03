440623d63d3f9abbe7e66440832e8db063379563
bishoe01 <bishoe01@gmail.com>
2022-08-03 23:18:44 +0900

    clear

---
+++ playground/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+{{TheMovie[0].title}}
+<TheCard/>
+<button type="button" class="btn btn-primary">Primary</button>
+</div>
+</template>
+
+<script>
+import TheMovie from './assets/moviedata.js';
+import TheCard from './components/Card.vue';
+export default {
+  name: 'App',
+  data() {
+    return {
+      TheMovie,
+    }
+  },
+  components: {
+    TheCard,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
